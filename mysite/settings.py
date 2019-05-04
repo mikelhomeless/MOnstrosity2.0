3a444110d18a5387c7bf2d9de9881e73f5f09f7e
mikelhomeless <mthomer1027@gmail.com>
2019-05-04 17:31:58 -0500

    apply git stash

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,10 +33,42 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+PIPELINE_CSS = {
+    'bootstrap': {
+        'source_filenames': (
+            'twitter_bootstrap/less/bootstrap.less',
+        ),
+        'output_filename': 'css/b.css',
+        'extra_context': {
+            'media': 'screen,projection',
+        },
+    },
+}
+
+PIPELINE_JS = {
+    'bootstrap': {
+        'source_filenames': (
+          'twitter_bootstrap/js/transition.js',
+          'twitter_bootstrap/js/modal.js',
+          'twitter_bootstrap/js/dropdown.js',
+          'twitter_bootstrap/js/scrollspy.js',
+          'twitter_bootstrap/js/tab.js',
+          'twitter_bootstrap/js/tooltip.js',
+          'twitter_bootstrap/js/popover.js',
+          'twitter_bootstrap/js/alert.js',
+          'twitter_bootstrap/js/button.js',
+          'twitter_bootstrap/js/collapse.js',
+          'twitter_bootstrap/js/carousel.js',
+          'twitter_bootstrap/js/affix.js',
+        ),
+        'output_filename': 'js/b.js',
+    },
+}
 
 # Application definition
 
 INSTALLED_APPS = [
+    'twitter_bootstrap',
     'nba.apps.NBAConfig',
     'django.contrib.admin',
     'django.contrib.auth',
