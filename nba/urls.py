b55cbfb157a6db2a8ae25dc133a800c35e5440d8
Dustin Klein <dbklein4@gmail.com>
2019-05-04 17:40:11 -0500

    Setup views (#4)
    * Added string methods to the models for ease of use
    
    * added simple views for players
    
    * Added a simple template to /players
    
    * added simple views for the games
    
    * added simple views for teams
    
    * untrack pycache

--- nba/urls.py
+++ nba/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')        
+    path('', views.index, name='index'),
+    path('players', views.players, name='players'),
+    path('players/<int:player_id>', views.player, name='player'),
+    path('games', views.games, name='games'),
+    path('games/<int:game_id>', views.game, name='game'),
+    path('teams', views.teams, name='teams'),
+    path('teams/<str:team_name>', views.team, name='team')   
 ]
